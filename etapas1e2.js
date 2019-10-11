4c85719bf05e91142433535ef5dadb17ac4e4dad
leticianasc <56457297+leticianasc@users.noreply.github.com>
2019-10-11 20:05:09 -0300

    Add files via upload

---
+++ etapas1e2.js
@@ -0,0 +1,40 @@
+var x = 27;
+
+var y = 470;
+
+
+function setup() {
+  
+createCanvas(500, 500);
+
+}
+
+
+function draw() {
+  
+background(270,66,60);
+  
+if (keyIsDown(LEFT_ARROW))
+    
+x-=5;
+
+  
+if (keyIsDown(RIGHT_ARROW))
+    
+x+=5;
+
+  
+if (keyIsDown(UP_ARROW))
+    
+y-=5;
+
+  
+if (keyIsDown(DOWN_ARROW))
+    
+y+=5;
+
+  
+ellipse(x, y, 50, 50);
+  
+rect(300, 445, 50, 50)
+}
